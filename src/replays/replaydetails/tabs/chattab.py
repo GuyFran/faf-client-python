50018ba118c3930fa571850e93a39a746b96b022
Gatsik <74517072+Gatsik@users.noreply.github.com>
2025-10-13 20:57:42 +0300

    Replaycard(ChatTab): QTableWidget -> QTableView for performance

--- src/replays/replaydetails/tabs/chattab.py
+++ src/replays/replaydetails/tabs/chattab.py
@@ -1,6 +1,10 @@
 from collections.abc import Generator
+from collections.abc import Sequence
+from typing import Any
 from typing import cast
 
+from PyQt6.QtCore import QAbstractTableModel
+from PyQt6.QtCore import QModelIndex
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction
 from PyQt6.QtGui import QCursor
@@ -13,8 +17,7 @@ from PyQt6.QtWidgets import QLabel
 from PyQt6.QtWidgets import QLineEdit
 from PyQt6.QtWidgets import QMenu
 from PyQt6.QtWidgets import QPushButton
-from PyQt6.QtWidgets import QTableWidget
-from PyQt6.QtWidgets import QTableWidgetItem
+from PyQt6.QtWidgets import QTableView
 from PyQt6.QtWidgets import QTextEdit
 from PyQt6.QtWidgets import QVBoxLayout
 from PyQt6.QtWidgets import QWidget
@@ -22,6 +25,33 @@ from PyQt6.QtWidgets import QWidget
 from src.config import Settings
 from src.replays.replaydetails.helpers import seconds_to_human
 from src.replays.replaydetails.replayreader import ReplayParser
+
+
+class ChatTableModel(QAbstractTableModel):
+    def __init__(self, chat_lines: Sequence[tuple[int, str, str, str, int]]) -> None:
+        super().__init__()
+        self.chat_lines = chat_lines
+
+    def rowCount(self, parent: QModelIndex = QModelIndex()) -> int:
+        return len(self.chat_lines)
+
+    def columnCount(self, parent: QModelIndex = QModelIndex()) -> int:
+        return 4
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int = 0) -> Any:
+
+        if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Horizontal:
+            return ("Time", "From", "To", "Message")[section]
+        return super().headerData(section, orientation, role)
+
+    def data(self, index: QModelIndex, role: int = 0) -> str | None:
+        row = index.row()
+        column = index.column()
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            if column == 0:
+                return str(seconds_to_human(self.chat_lines[row][column] // 10))
+            return cast(str, self.chat_lines[row][column])
 
 
 class ChatTabUI:
@@ -58,19 +88,12 @@ class ChatTabUI:
         search_layout.addWidget(self.alternateColorsCheckBox)
         layout.addLayout(search_layout)
 
-        self.chatTable = QTableWidget()
+        self.chatTable = QTableView()
         self.chatTable.setVerticalScrollMode(self.chatTable.ScrollMode.ScrollPerPixel)
         self.chatTable.setEditTriggers(self.chatTable.EditTrigger.NoEditTriggers)
         self.chatTable.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         self.chatTable.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         self.chatTable.horizontalHeader().setStretchLastSection(True)
-        self.chatTable.setColumnCount(4)
-        self.chatTable.setHorizontalHeaderLabels((
-            "Time",
-            "From",
-            "To",
-            "Message",
-        ))
         layout.addWidget(self.chatTable)
 
 
@@ -82,8 +105,8 @@ class ChatTab(QWidget):
         self.ui.searchInput.textChanged.connect(self.search)
         self.ui.searchInput.returnPressed.connect(self.search_next)
         self.ui.clearButton.clicked.connect(self.ui.searchInput.clear)
-        self.ui.chatTable.itemDoubleClicked.connect(self.on_chat_item_double_clicked)
-        self.ui.chatTable.itemPressed.connect(self.on_chat_item_clicked)
+        self.ui.chatTable.doubleClicked.connect(self.on_chat_item_double_clicked)
+        self.ui.chatTable.pressed.connect(self.on_chat_item_clicked)
 
         self.alternate_row_colors = Settings.get(
             "replaycard.chat/alternate_row_colors",
@@ -116,36 +139,27 @@ class ChatTab(QWidget):
         self.message_filters.buttonToggled.connect(self.on_message_filter_changed)
 
         self._search_index = 0
-        self._matching_items: list[QTableWidgetItem] = []
+        self._matching_indexes: list[QModelIndex] = []
 
         self.chat_lines: list[tuple[int, str, str, str, int]] = []
 
-    def initialize(self, replay: ReplayParser) -> None:
-        self.chat_lines = replay.chatLine
+    def initialize(self, parser: ReplayParser) -> None:
+        self.chat_lines = parser.chatLine
         self.ui.searchInput.clear()
-        self.ui.chatTable.clear()
-        self.fill_chat()
+        model = ChatTableModel(self.chat_lines)
+        self.ui.chatTable.setModel(model)
 
-    def fill_chat(self) -> None:
-        self.ui.chatTable.setRowCount(len(self.chat_lines))
         btn_ids = {
             btn.text(): self.message_filters.id(btn)
             for btn in self.message_filters.buttons()
         }
         for row, line in enumerate(self.chat_lines):
-            for col, elem in enumerate(line[:-1]):
-                if col == 0:
-                    text = seconds_to_human(cast(int, elem) // 10)
-                else:
-                    text = elem
-                self.ui.chatTable.setItem(row, col, QTableWidgetItem(text))
-                if col == 2:
-                    hide = not self.visible_recipients[btn_ids[cast(str, text)]]
-                    self.ui.chatTable.setRowHidden(row, hide)
+            hide = not self.visible_recipients[btn_ids[line[2]]]
+            self.ui.chatTable.setRowHidden(row, hide)
 
     def _reset_search(self) -> None:
-        self.ui.chatTable.setCurrentItem(None)
-        self._matching_items.clear()
+        self.ui.chatTable.setCurrentIndex(QModelIndex())
+        self._matching_indexes.clear()
         self._search_index = 0
 
     def search(self, text: str) -> None:
@@ -154,27 +168,35 @@ class ChatTab(QWidget):
         if not text:
             return
 
-        self._matching_items = [
-            item for item in
-            self.ui.chatTable.findItems(text, Qt.MatchFlag.MatchContains)
-            if not self.ui.chatTable.isRowHidden(item.row())
+        model = cast(ChatTableModel, self.ui.chatTable.model())
+        self._matching_indexes = [
+            index
+            for column in range(model.columnCount())
+            for index in model.match(
+                model.index(0, column),
+                Qt.ItemDataRole.DisplayRole,
+                text,
+                -1,
+                Qt.MatchFlag.MatchContains,
+            )
+            if not self.ui.chatTable.isRowHidden(index.row())
         ]
-        if self._matching_items:
-            self.ui.chatTable.setCurrentItem(self._matching_items[0])
+        if self._matching_indexes:
+            self.ui.chatTable.setCurrentIndex(self._matching_indexes[0])
 
     def search_next(self) -> None:
-        if not self._matching_items:
+        if not self._matching_indexes:
             if text := self.ui.searchInput.text():
                 self.search(text)
             return
         modifiers = QApplication.keyboardModifiers()
         self._search_index += -1 if modifiers == Qt.KeyboardModifier.ShiftModifier else 1
-        item = self._matching_items[self._search_index % len(self._matching_items)]
-        self.ui.chatTable.setCurrentItem(item)
+        index = self._matching_indexes[self._search_index % len(self._matching_indexes)]
+        self.ui.chatTable.setCurrentIndex(index)
 
     def on_message_filter_changed(self, button: QCheckBox) -> None:
         self.visible_recipients[self.message_filters.id(button)] = button.isChecked()
-        for row in range(self.ui.chatTable.rowCount()):
+        for row in range(len(self.chat_lines)):
             if button.text() == self.chat_lines[row][2]:
                 self.ui.chatTable.setRowHidden(row, not button.isChecked())
         self._reset_search()
@@ -188,15 +210,15 @@ class ChatTab(QWidget):
         Settings.set("replaycard.chat/visible_recipients", self.visible_recipients)
         Settings.set("replaycard.chat/alternate_row_colors", self.alternate_row_colors)
 
-    def on_chat_item_double_clicked(self, chat_item: QTableWidgetItem) -> None:
+    def on_chat_item_double_clicked(self, index: QModelIndex) -> None:
         if QApplication.mouseButtons() != Qt.MouseButton.LeftButton:
             return
         text_dialog = QDialog(self)
-        timing, sender_login, to, *_ = self.chat_lines[chat_item.row()]
+        timing, sender_login, to, *_ = self.chat_lines[index.row()]
         title = f"[{seconds_to_human(timing // 10)}] {sender_login} to {to}"
         text_dialog.setWindowTitle(title)
         layout = QVBoxLayout(text_dialog)
-        text_edit = QTextEdit(chat_item.text())
+        text_edit = QTextEdit(index.data(Qt.ItemDataRole.DisplayRole))
         text_edit.setReadOnly(True)
         layout.addWidget(text_edit)
         text_dialog.exec()
@@ -204,21 +226,22 @@ class ChatTab(QWidget):
 
     def copy_text_in_selection(self) -> None:
         def _selected_text() -> Generator[str]:
-            selection = self.ui.chatTable.selectedItems()
+            selection = self.ui.chatTable.selectedIndexes()
             if not selection:
                 return
-            for i, table_item in enumerate(selection[:-1]):
-                if selection[i + 1].row() > table_item.row():
-                    yield f"{table_item.text()}\n"
+            for i, model_index in enumerate(selection[:-1]):
+                text = model_index.data(Qt.ItemDataRole.DisplayRole)
+                if selection[i + 1].row() > model_index.row():
+                    yield f"{text}\n"
                 else:
-                    yield f"{table_item.text()};"
-            yield selection[-1].text()
+                    yield f"{text};"
+            yield selection[-1].data(Qt.ItemDataRole.DisplayRole)
 
         clip = QApplication.clipboard()
         if clip is not None:
             clip.setText("".join(_selected_text()))
 
-    def on_chat_item_clicked(self, _: QTableWidgetItem) -> None:
+    def on_chat_item_clicked(self, _: QModelIndex) -> None:
         if QApplication.mouseButtons() != Qt.MouseButton.RightButton:
             return
         menu = QMenu(self.ui.chatTable)
