805559b8b7bb853186793e3970494ef0c3538441
Gatsik <74517072+Gatsik@users.noreply.github.com>
2025-11-30 01:15:14 +0200

    Update client_id for testing environment
    with respect to 58464c4ae427a049deff5c0501631c46e6425daa

--- src/config/testing.py
+++ src/config/testing.py
@@ -3,4 +3,4 @@ from .production import default_values as production_defaults
 default_values = production_defaults.copy()
 default_values['display_name'] = 'Test Server'
 default_values['host'] = 'faforever.xyz'
-default_values['oauth/client_id'] = 'faf-java-client'
+default_values['oauth/client_id'] = '95ecec08-29c1-4c48-ae0a-b000ff349cb8'
