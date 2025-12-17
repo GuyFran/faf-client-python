e95eacbfbfa8372aa7611943fbd5cdb4e6c912cb
Gatsik <74517072+Gatsik@users.noreply.github.com>
2025-12-18 00:11:46 +0200

    Statswidget: Remove unused leagues code

--- src/stats/_statswidget.py
+++ src/stats/_statswidget.py
@@ -2,7 +2,6 @@ import logging
 import time
 
 from PyQt6 import QtCore
-from PyQt6 import QtWidgets
 
 from src import util
 from src.api.models.Leaderboard import Leaderboard
@@ -33,7 +32,6 @@ class StatsWidget(BaseClass, FormClass, BusyWidget):
 
         self.selected_player = None
         self.selected_player_loaded = False
-        self.leagues.currentChanged.connect(self.leagueUpdate)
         self.currentChanged.connect(self.busy_entered)
         self.pagesDivisions = {}
         self.pagesDivisionsResults = {}
@@ -82,158 +80,6 @@ class StatsWidget(BaseClass, FormClass, BusyWidget):
         self.setStyleSheet(
             util.THEME.readstylesheet("stats/formatters/style.css"),
         )
-
-    @QtCore.pyqtSlot(int)
-    def leagueUpdate(self, index):
-        self.currentLeague = index + 1
-        leagueTab = self.leagues.widget(index).findChild(
-            QtWidgets.QTabWidget, "league" + str(index),
-        )
-        if leagueTab:
-            if leagueTab.currentIndex() == 0:
-                if time.time() - self.floodtimer > ANTIFLOOD:
-                    self.floodtimer = time.time()
-                    self.client.statsServer.send(
-                        dict(
-                            command="stats",
-                            type="league_table",
-                            league=self.currentLeague,
-                        ),
-                    )
-
-    @QtCore.pyqtSlot(int)
-    def divisionsUpdate(self, index):
-        if index == 0:
-            if time.time() - self.floodtimer > ANTIFLOOD:
-                self.floodtimer = time.time()
-                self.client.statsServer.send(
-                    dict(
-                        command="stats",
-                        type="league_table",
-                        league=self.currentLeague,
-                    ),
-                )
-
-        elif index == 1:
-            tab = self.currentLeague - 1
-            if tab not in self.pagesDivisions:
-                self.client.statsServer.send(
-                    dict(
-                        command="stats",
-                        type="divisions",
-                        league=self.currentLeague,
-                    ),
-                )
-
-    @QtCore.pyqtSlot(int)
-    def divisionUpdate(self, index):
-        if time.time() - self.floodtimer > ANTIFLOOD:
-            self.floodtimer = time.time()
-            self.client.statsServer.send(
-                dict(
-                    command="stats",
-                    type="division_table",
-                    league=self.currentLeague,
-                    division=index,
-                ),
-            )
-
-    def createDivisionsTabs(self, divisions):
-        userDivision = ""
-        me = self.client.me.player
-        if me.league is not None:  # was me.division, but no there there
-            userDivision = me.league[1]  # ? [0]=league and [1]=division
-
-        pages = QtWidgets.QTabWidget()
-
-        foundDivision = False
-
-        for division in divisions:
-            name = division["division"]
-            index = division["number"]
-            league = division["league"]
-            widget = QtWidgets.QTextBrowser()
-
-            if league not in self.pagesDivisionsResults:
-                self.pagesDivisionsResults[league] = {}
-
-            self.pagesDivisionsResults[league][index] = widget
-
-            pages.insertTab(index, widget, name)
-
-            if name == userDivision:
-                foundDivision = True
-                pages.setCurrentIndex(index)
-                self.client.statsServer.send(
-                    dict(
-                        command="stats",
-                        type="division_table",
-                        league=league,
-                        division=index,
-                    ),
-                )
-
-        if not foundDivision:
-            self.client.statsServer.send(
-                dict(
-                    command="stats",
-                    type="division_table",
-                    league=league,
-                    division=0,
-                ),
-            )
-
-        pages.currentChanged.connect(self.divisionUpdate)
-        return pages
-
-    def createResults(self, values, table):
-
-        formatter = self.FORMATTER_LADDER
-        formatter_header = self.FORMATTER_LADDER_HEADER
-        glist = []
-        append = glist.append
-        append(
-            "<table style='color:#3D3D3D' cellspacing='0' cellpadding='4' "
-            "width='100%' height='100%'><tbody>",
-        )
-        append(
-            formatter_header.format(
-                rank="rank", name="name", score="score", color="#92C1E4",
-            ),
-        )
-
-        for val in values:
-            rank = val["rank"]
-            name = val["name"]
-            score = str(val["score"])
-            if self.client.login == name:
-                append(
-                    formatter.format(
-                        rank=str(rank), name=name, score=score, color="#6CF",
-                    ),
-                )
-            elif rank % 2 == 0:
-                append(
-                    formatter.format(
-                        rank=str(rank), name=name,
-                        score=str(val["score"]), color="#F1F1F1",
-                    ),
-                )
-            else:
-                append(
-                    formatter.format(
-                        rank=str(rank), name=name,
-                        score=str(val["score"]), color="#D8D8D8",
-                    ),
-                )
-
-        append("</tbody></table>")
-        html = "".join(glist)
-
-        table.setHtml(html)
-
-        table.verticalScrollBar().setValue(table.verticalScrollBar().minimum())
-        return table
 
     @QtCore.pyqtSlot(int)
     def leaderboardsTabChanged(self, curr):
